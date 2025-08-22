e76f1fb628b7a00ab4881d1b524a5419d803d80c
Rahul Raveendran <rahulprgrmr@gmail.com>
2025-08-22 10:17:23 +0530

    Auto session plugin updates

--- .config/nvim/lua/plugins/sessions.lua
+++ .config/nvim/lua/plugins/sessions.lua
@@ -1,28 +1,57 @@
 return {
 	"rmagatti/auto-session",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope.nvim",
-	},
 	lazy = false,
-	config = function()
-		require("auto-session").setup({
-			session_lens = {
-				load_on_setup = true, -- Initialize on startup (requires Telescope)
-				theme_conf = { -- Pass through for Telescope theme options
-					-- layout_config = { -- As one example, can change width/height of picker
-					--   width = 0.8,    -- percent of window
-					--   height = 0.5,
-					-- },
-				},
-				previewer = false, -- File preview for session picker
+	keys = {
+		-- Will use Telescope if installed or a vim.ui.select picker otherwise
+		{ "<leader>ss", "<cmd>SessionSearch<CR>", desc = "Session search" },
+		-- { "<leader>ws", "<cmd>SessionSave<CR>", desc = "Save session" },
+		-- { "<leader>wa", "<cmd>SessionToggleAutoSave<CR>", desc = "Toggle autosave" },
+	},
+
+	---enables autocomplete for opts
+	---@module "auto-session"
+	---@type AutoSession.Config
+	opts = {
+		-- The following are already the default values, no need to provide them if these are already the settings you want.
+		session_lens = {
+			picker = "fzf", -- "telescope"|"snacks"|"fzf"|"select"|nil Pickers are detected automatically but you can also manually choose one. Falls back to vim.ui.select
+			mappings = {
+				-- Mode can be a string or a table, e.g. {"i", "n"} for both insert and normal mode
+				delete_session = { "i", "<C-D>" },
+				alternate_session = { "i", "<C-S>" },
+				copy_session = { "i", "<C-Y>" },
 			},
-			vim.keymap.set(
-				"n",
-				"<leader>ss",
-				require("auto-session.session-lens").search_session,
-				{ noremap = true, desc = "Search session" }
-			),
-		})
-	end,
+
+			picker_opts = {
+				-- For Telescope, you can set theme options here, see:
+				-- https://github.com/nvim-telescope/telescope.nvim/blob/master/doc/telescope.txt#L112
+				-- https://github.com/nvim-telescope/telescope.nvim/blob/master/lua/telescope/themes.lua
+				--
+				-- border = true,
+				-- layout_config = {
+				--   width = 0.8, -- Can set width and height as percent of window
+				--   height = 0.5,
+				-- },
+
+				-- For Snacks, you can set layout options here, see:
+				-- https://github.com/folke/snacks.nvim/blob/main/docs/picker.md#%EF%B8%8F-layouts
+				--
+				-- preset = "dropdown",
+				-- preview = false,
+				-- layout = {
+				--   width = 0.4,
+				--   height = 0.4,
+				-- },
+
+				-- For Fzf-Lua, picker_opts just turns into winopts, see:
+				-- https://github.com/ibhagwan/fzf-lua#customization
+				--
+				height = 0.8,
+				width = 0.50,
+			},
+
+			-- Telescope only: If load_on_setup is false, make sure you use `:SessionSearch` to open the picker as it will initialize everything first
+			load_on_setup = true,
+		},
+	},
 }
